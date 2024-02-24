dbddbdb61d4f2a87114e45cf5e2c87fe38ad7a2e
Yuxuanzzz <93758547+Yuxuanzzz@users.noreply.github.com>
2024-02-24 12:48:01 -0800

    Update ConfigTypes.hpp / remove yaml initialization

--- 3d/C++/includes/ConfigTypes.hpp
+++ 3d/C++/includes/ConfigTypes.hpp
@@ -1,4 +1,3 @@
-// ConfigTypes.hpp
 #ifndef CONFIGTYPES_HPP
 #define CONFIGTYPES_HPP
 #include "Cell.hpp"
@@ -11,19 +10,18 @@ public:
     int padding = 0;
     float zScaling = 1;
     float blurSigma = 0;
+
+    //SimulationConfig(const YAML::Node& node) {
+    //    iterationsPerCell = node["iterations_per_cell"].as<int>();
+    //    backgroundColor = node["background_color"].as<float>();
+    //    cellColor = node["cell_color"].as<float>();
+    //    padding = node["padding"].as<int>();
+    //    zScaling = node["z_scaling"].as<float>();
+    //    blurSigma = node["blur_sigma"].as<float>();
+    //}
 private:
     int zSlices = -1;
     std::vector<int> zValues;
-
-    SimulationConfig(const YAML::Node& node) {
-        iterationsPerCell = node["iterations_per_cell"].as<int>();
-        backgroundColor = node["background_color"].as<float>();
-        cellColor = node["cell_color"].as<float>();
-        padding = node["padding"].as<int>();
-        zScaling = node["z_scaling"].as<float>();
-        blurSigma = node["blur_sigma"].as<float>();
-
-    }
     // No need for C++
     //void checkZValues() const {
     //    if (!zValues.empty()) {
@@ -45,22 +43,23 @@ public:
 
     static void checkProbability(std::vector<float> values) {//TODO
     	}
-    ProbabilityConfig(const YAML::Node& node)
-    {
-        perturbation = node["perturbation"].as<float>();
-        split = node["split"].as<float>();
-    }
+    //ProbabilityConfig(const YAML::Node& node)
+    //{
+    //    perturbation = node["perturbation"].as<float>();
+    //    split = node["split"].as<float>();
+    //}
 };
 
 class BaseConfig {
 public:
-    CellConfig cell;
+    std::unique_ptr<CellConfig> cell; //unique ptr to cell config
     char CellType; //Added new attribute for type check: 'b'/'s' stands for bacilli/sphere
     SimulationConfig simulation;
     ProbabilityConfig prob;
-    BaseConfig(const YAML::Node& node) :
-        simulation(node), prob(node), cell(node) {
-        // Parse YAML node and initialize config object
-    }
+
+    //BaseConfig(const YAML::Node& node) :
+    //    simulation(node), prob(node), cell(node) {
+    //    // Parse YAML node and initialize config object
+    //}
 };
 #endif
